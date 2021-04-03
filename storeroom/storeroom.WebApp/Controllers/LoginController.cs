13e9983717b1c65a23b2c2decd19cbf8867d3cb9
Quansato <ASUS@LAPTOP-UTNLUO6F>
2021-04-03 12:58:30 +0700

    .

---
+++ storeroom/storeroom.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using storeroom.Application.Catalog.Users.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeroom.WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
